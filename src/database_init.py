6539c6e2d8ef42126584739cd749e247a9be2994
Ryan <e0310273@u.nus.edu>
2021-01-30 11:24:56 +0800

    added warning for db init

--- src/database_init.py
+++ src/database_init.py
@@ -1,7 +1,16 @@
+# RUNNING THIS WILL DELETE ALL DATA IN DATABASE
+
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 from reqparsers import initializeParsers
+import sys
+
+yn = input("Warning: This will delete all database entries and create new db file. type y to continue and n to exit ->")
+
+if yn != "y":
+    print("exiting")
+    sys.exit()
 
 app  = Flask(__name__)
 api = Api(app)
@@ -18,8 +27,3 @@ class FriendsModel(db.Model):
     friend_id = db.Column(db.String, nullable = False)
 
 db.create_all()
-
-# api.add_resource(, "/customer/<int:customer_id>")
-
-if __name__ == "__main__":
-    app.run(debug=True)
